0198dbb938f385c3da6225c085fea7ea1a3f46e8
Omar <obmargit@protonmail.com>
2021-11-25 20:15:58 -0500

    Defined class Snake

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+import time
 from turtle import Turtle, Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
 screen.title('Snake Game')
+screen.tracer(0)
+
 # Create a snake body -create 3 snake bodies starting from 0,0 and going back, body is 20x20
 starting_pos = [(0, 0), (-20, 0), (-40, 0)]
-# for snakes in range(0, 3):
-#     snake_body = Turtle(shape='square')
-#     snake_body.color('white')
-#     snake_body.goto(x=x_pos, y=0)
-#     x_pos -= 20
-for position in starting_pos:
-    segment = Turtle(shape='square')
-    segment.color('white')
-    segment.goto(position)
+segments = []
+
+snake = Snake()
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
 # segment_2 = Turtle(shape='square')
 # segment_2.color('white')
---
+++ snake.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+
+
+class Snake:
+    def __init__(self):
+        self.starting_pos = [(0, 0), (-20, 0), (-40, 0)]
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in self.starting_pos:
+            segment = Turtle(shape='square')
+            segment.color('white')
+            segment.penup()
+            segment.goto(position)
+            self.segments.append(segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
